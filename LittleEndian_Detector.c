fad41b8d37b874505aa2be7eb1397e2124c2bcde
Ji Ho Bok <treehouse31@naver.com>
2017-11-27 00:51:25 +0900

    little-endian: Modify main function

--- LittleEndian_Detector.c
+++ LittleEndian_Detector.c
@@ -34,12 +34,15 @@ int main()
 	printf("This program is intended to determine whether this computer is little endian or not.\n\n");
 	printf("Result: ");
 	
-	/*
-	In accordance with the result of the functino, Test_Little_Endian()
-	we will print a message..
-	The_Little_endian message will return 1, in case of little endian..
-	The_Little_endian message will return 0, in case of little endian..
-	*/
+	if(Test_Little_Endian())
+	{
+		printf("This pc is on the little endian.\n");
+	}
+	else
+	{
+		printf("This pc is on the big endian.\n");
+	}
+	getchar();
 	return 0;
 }
 
